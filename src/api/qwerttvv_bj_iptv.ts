8183ec218c19e52b70f9af244018ed081a1a9d42
qq2236639958 <28900094+HeFung@users.noreply.github.comL>
2024-04-06 00:02:02 +0800

    add

--- src/api/qwerttvv_bj_iptv.ts
+++ src/api/qwerttvv_bj_iptv.ts
@@ -3,25 +3,25 @@ import type { TSources } from "../types"
 
 export const qwerttvv_bj_iptv_sources: TSources = [
   {
-    name: "qwerttvv/Beijing-IPTV IPTV Unicom",
+    name: "北京联通 单播列表",
     f_name: "q_bj_iptv_unicom",
     url: "https://raw.githubusercontent.com/qwerttvv/Beijing-IPTV/master/IPTV-Unicom.m3u",
     filter: filter,
   },
   {
-    name: "qwerttvv/Beijing-IPTV IPTV Unicom Multicast",
+    name: "北京联通 组播列表",
     f_name: "q_bj_iptv_unicom_m",
     url: "https://raw.githubusercontent.com/qwerttvv/Beijing-IPTV/master/IPTV-Unicom-Multicast.m3u",
     filter: filter,
   },
   {
-    name: "qwerttvv/Beijing-IPTV IPTV Mobile",
+    name: "北京移动 单播列表",
     f_name: "q_bj_iptv_mobile",
     url: "https://raw.githubusercontent.com/qwerttvv/Beijing-IPTV/master/IPTV-Mobile.m3u",
     filter: filter,
   },
   {
-    name: "qwerttvv/Beijing-IPTV IPTV Mobile Multicast",
+    name: "北京移动 组播列表",
     f_name: "q_bj_iptv_mobile_m",
     url: "https://raw.githubusercontent.com/qwerttvv/Beijing-IPTV/master/IPTV-Mobile-Multicast.m3u",
     filter: filter,
